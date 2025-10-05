2db3e12601eb8a3931bdc4daa93e123ac242cf77
Dea Sustaita <deasustaita@gmail.com>
2025-10-04 22:37:22 -0400

    adjusted wolf ai, and tested with actual player

--- AI_Stuff/Scenes/Enemies/wolf_ai.gd
+++ AI_Stuff/Scenes/Enemies/wolf_ai.gd
@@ -1,40 +1,58 @@
 extends Node2D
 
-@export var speed = 50
+@export var normal_speed = 125
 @export var direction = -1
+
+@export var y_value = 0
+@export var left_x = -300
+@export var right_x = 300
+
+
+func _ready():
+	$Wolf.global_position.y = y_value
+	$Walls/LeftWall/LW_Collision.global_position.y = y_value
+	$Walls/RightWall/RW_Collision.global_position.y = y_value
+	$Walls/LeftWall/LW_Collision.global_position.x = left_x
+	$Walls/RightWall/RW_Collision.global_position.x = right_x
+	$Wolf.global_position.x = (left_x + right_x) / 2.0
+	
 
 func _physics_process(_delta):
 	$Wolf/AnimationPlayer.play("Walking_wolf")
+	var speed = normal_speed
 	
+	# change speed if the wolf sees the player
+	if $Wolf/LineOfSight.is_colliding() and $Wolf/LineOfSight.get_collider(0).is_class("CharacterBody2D"):
+		speed = normal_speed*2
+		$Wolf/AnimationPlayer.speed_scale = 2.5
+	else:
+		$Wolf/AnimationPlayer.speed_scale = 1
+		
+		# Makes sure the lines of sight don't go past the area they're supposed to be roaming
+		if $Wolf/LineOfSight.is_colliding():
+			if $Wolf/LineOfSight.get_collider(0) == $Walls/LeftWall:
+				$Wolf/LineOfSight.target_position.x += ($Walls/LeftWall/LW_Collision.global_position.x - $Wolf/LineOfSight.global_position.x) - $Wolf/LineOfSight.target_position.x + $Walls/LeftWall/LW_Collision.shape.size.x			
+			elif $Wolf/LineOfSight.get_collider(0) == $Walls/RightWall:
+				$Wolf/LineOfSight.target_position.x -= $Walls/RightWall/RW_Collision.global_position.x - ($Wolf/LineOfSight.global_position.x - $Wolf/LineOfSight.target_position.x) - $Walls/RightWall/RW_Collision.shape.size.x
+
 	if direction == 1 and $Walls/RightWall.has_overlapping_bodies():
 		direction = -1
 		$Wolf.scale.x = -1
+		$Wolf/LineOfSight.target_position.x = -200
 	elif direction == -1 and $Walls/LeftWall.has_overlapping_bodies():
 		direction = 1
 		$Wolf.scale.x = -1
-	
-	# change speed if the wolf sees the player
-	if $Wolf/LineOfSight.is_colliding():
-		speed = 125
-		$Wolf/AnimationPlayer.speed_scale = 2.5
-	else:
-		speed = 50
-		$Wolf/AnimationPlayer.speed_scale = 1
+		$Wolf/LineOfSight.target_position.x = -200
 		
-	if $Wolf/BackSight.is_colliding():
-		direction = direction * -1
+	# Makes the wolf swiftly turn around if the player is too close, unless they are beyond the bounds of the wolf's roaming
+	if $Wolf/BackSight.is_colliding() and ($Wolf/BackSight.get_collider(0).global_position.x >= $Walls/LeftWall/LW_Collision.global_position.x and $Wolf/BackSight.get_collider(0).global_position.x <= $Walls/RightWall/RW_Collision.global_position.x):
+		direction *= -1
 		$Wolf.scale.x = -1
+		$Wolf/LineOfSight.target_position.x = -200
 	
 	
 	$Wolf.velocity.x = direction * speed
 	if $Wolf.velocity.y != 0:
 		$Wolf.velocity.y = 0
+	$Wolf.global_position.y = y_value
 	$Wolf.move_and_slide()
-
-
-# allow wolf to start walking
-# check to see if player in front (5-6) or behind (2-3)
-# if behind (if location more or less than player)
-	# change direction
-# speed up and charge towards player
-# if player caught: game over (respawn)
--- AI_Stuff/Scenes/EnvironmentThings/bush.gd
+++ AI_Stuff/Scenes/EnvironmentThings/bush.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
-
+var body_inside = false
+var hide_player = false
+var my_body
 
 func _on_area_2d_body_entered(body):
-	# if E is pressed 
-	# make the player not visible and tangible to the enemies
-	# if E is pressed again undo those things
-	pass 
+	body_inside = true
+	my_body = body
+
+func _on_area_2d_body_exited(_body):
+	body_inside = false
+	
+func _process(_delta):
+	if body_inside:
+		if Input.is_key_pressed(KEY_E):
+			hide_player = true
+		else:
+			hide_player = false
+	
+	if hide_player and $Area2D.get_overlapping_bodies()[0] == my_body:
+		pass # make invisible and make sure can't move
+	elif $Area2D.has_overlapping_bodies(): 
+		pass # make visible and make sure can move
