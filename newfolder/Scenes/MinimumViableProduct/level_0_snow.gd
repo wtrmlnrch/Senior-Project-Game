e09b15c91e499bc7ed687e1ebaa5d63f93b58501
Dea Sustaita <deasustaita@gmail.com>
2025-10-06 21:38:24 -0400

    adding a space so that i can push to release

--- newfolder/Scenes/MinimumViableProduct/level_0_snow.gd
+++ newfolder/Scenes/MinimumViableProduct/level_0_snow.gd
@@ -13,3 +13,4 @@ func _ready():
 	camera.limit_top = used_rect.position.y * cell_size.y
 	camera.limit_right = (used_rect.position.x + used_rect.size.x) * cell_size.x
 	camera.limit_bottom = (used_rect.position.y + used_rect.size.y) * cell_size.y
+ 
