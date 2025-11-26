d1f02ad00e90daea9cc51a752e5beec3b3e1e25a
Dea Sustaita <deasustaita@gmail.com>
2025-11-26 15:14:27 -0500

    created a simple squirrel ai, which at this point does not connect to anything.

---
+++ AI_Stuff/Scenes/Enemies/squirrel_ai.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var horizontal_speed = 200
+@export var vertical_speed = 25
+
+@export var running_duration = 1.0
+
+var speed = Vector2()
+var running_time = 0.0
+var running = false
+
+var waiting_time = 1.0
+
+func _ready():
+	$Squirrel/AnimationPlayer.play("Scanning")
+
+func _physics_process(delta):
+	if $Squirrel/ShapeCast2D.is_colliding() and $Squirrel/ShapeCast2D.get_collider(0).get_name() == "CharacterBody2D" and !running:
+		
+		running_time = running_duration
+		running = true
+		
+		var hori_multi = 1
+		
+		# make the squirrel make a noise then wait and disappear 		
+		if $Squirrel/ShapeCast2D.get_collider(0).global_position.x < $Squirrel.global_position.x:
+			$Squirrel/AnimationPlayer.play("Running Right")
+		else:
+			hori_multi = -1
+			$Squirrel/AnimationPlayer.play("Running Left")
+		speed = Vector2(horizontal_speed*hori_multi, vertical_speed)
+	
+	if (running_time > 0):
+		$Squirrel.velocity = speed
+		running_time -= delta
+	elif (running == true):
+		queue_free()
+		
+	$Squirrel.move_and_slide()
