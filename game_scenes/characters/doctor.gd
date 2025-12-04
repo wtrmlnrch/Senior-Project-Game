3cc75120d105bbd3584ce2699aa2dd82d0bc93f3
Stormium04 <stormystorm04@gmail.com>
2025-12-04 17:25:28 -0500

    Fixed the error in dialogue not starting

--- game_scenes/characters/doctor.gd
+++ game_scenes/characters/doctor.gd
@@ -1,31 +1,21 @@
 extends npc
-
 var player_in_range = false
-@export var dialogic_timeline: String = "doctalk"
-
+@export var dialogic_timeline: String = "res://Dialogue stuff/doctalk.dtl"
 func _ready():
-	npc_name = "Brother Land"
-	#$Area2D.area_entered.connect(_on_area_entered)
-	#$Area2D.area_exited.connect(_on_area_exited)
+	npc_name = "Dr.Land"
+	$"talk-area".body_entered.connect(_on_area_entered)
+	$"talk-area".body_exited.connect(_on_area_exited)
 	
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("Interact"):
 		interact()
-
-
 func _on_area_entered(area):
 	player_in_range = true
-
-
 func _on_area_exited(area):
 	player_in_range = false
-
-
 func interact():
 	if player_in_range:
 		Dialogic.start(dialogic_timeline)
 		special_talk_function()
-
-
 func special_talk_function():
 	pick_inventory_slot('pill')
