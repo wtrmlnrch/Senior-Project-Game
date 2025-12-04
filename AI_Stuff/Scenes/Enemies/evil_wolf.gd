b999b8056411af26e75ae41e86836b8205a00a0d
Dea Sustaita <deasustaita@gmail.com>
2025-12-03 23:39:45 -0500

    commenting implementation ideas

---
+++ AI_Stuff/Scenes/Enemies/evil_wolf.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var nav_agent = $"Evil Wolf/NavigationAgent2D"
+
+# make a function to "generate the path for the wolf to follow"
+
+# make another function to adjust that path based on other wolves?
+
+# function to adjust with the addition of the player character
+
+# look at the navAgent and the navRegion for information and the starred video on my google
+
+# wolves should follow a set path, patrolling around the areas
+# when seeing a player they should start moving towards it
+# make a random chance for it to howl
+# howl then causes the function to be called again to adjust wolf paths
---
+++ AI_Stuff/Scenes/Enemies/evil_wolf_ai.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+
+# take the number of wolves and set them on their navigating way
