901c96c0b68b37291245047d7d8ddfb38e83c4a8
Stormium04 <stormystorm04@gmail.com>
2025-12-09 14:47:03 -0500

    Daniel/Robert Dialogue Works

--- game_scenes/characters/daniel.gd
+++ game_scenes/characters/daniel.gd
@@ -1,29 +1,36 @@
 extends npc
 
 var player_in_range = false
-@export var dialogic_timeline: String = "res://Dialogue stuff/butchertalk.dtl"
+var dialogue_active = false
+@export var dialogic_timeline: String = "res://Assets/NPCS/Villagers/leader/butchertalk.dtl"
 
 func _ready():
 	npc_name = "Robert"
 	$"talk-area".body_entered.connect(_on_area_entered)
 	$"talk-area".body_exited.connect(_on_area_exited)
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	
 func _process(delta):
-	if player_in_range and Input.is_action_just_pressed("Interact"):
+	if player_in_range and Input.is_action_just_pressed("Interact") and not dialogue_active:
 		interact()
 
 func _on_area_entered(body):
-	if body.name == "MainCharacter":
+	if body.name == "MainCharacter" and body != self:
 		player_in_range = true
 
 func _on_area_exited(body):
-	if body.name == "MainCharacter":
+	if body.name == "MainCharacter" and body != self:
 		player_in_range = false
 
 func interact():
-	if player_in_range:
+	if player_in_range and not dialogue_active:
+		dialogue_active = true
 		Dialogic.start(dialogic_timeline)
 		special_talk_function()
+
+func _on_dialogic_signal(argument: String):
+	if argument == "dialogue_end":
+		dialogue_active = false
 
 func special_talk_function():
 	pass
