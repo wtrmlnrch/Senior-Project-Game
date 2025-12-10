ee310ef5a84d38121f3ca46e43dc3e0f731e5b23
aallen764 <aubr3yreyne@gmail.com>
2025-12-09 22:45:30 -0500

    added collisions for forest level

---
+++ newfolder/forest_night.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+@onready var tilemap = $ground
+@onready var player = $MainCharacter
+@onready var camera = $MainCharacter/PlayerCamera
+
+func _ready():
+	var used_rect = tilemap.get_used_rect()
+	var cell_size = tilemap.tile_set.tile_size
+	
+	camera.limit_left = used_rect.position.x * cell_size.x
+	camera.limit_top = used_rect.position.y * cell_size.y
+	camera.limit_right = (used_rect.position.x + used_rect.size.x) * cell_size.x
+	camera.limit_bottom = (used_rect.position.y + used_rect.size.y) * cell_size.y
+	
+	player.max_speed = 65
+	player.speed = player.max_speed
+	camera.zoom = Vector2(2.50, 2.50)
