ad728f1d77ee9cc52ca2f189be6a32a97c4c78c7
Stormium04 <stormystorm04@gmail.com>
2025-12-09 23:36:01 -0500

    added opening cinematic

--- game_scenes/interiors/clinic.gd
+++ game_scenes/interiors/clinic.gd
@@ -2,19 +2,18 @@ extends Node2D
 
 @onready var camera = $MainCharacter/PlayerCamera
 @onready var doctor = $Doctor
-
 var scene = load("res://newfolder/village.tscn")
 
 func _ready():	
+	Dialogic.start("res://Dialogue stuff/opening.dtl")
+	await Dialogic.timeline_ended
 	Dialogic.start("waking")
+	
 	camera.limit_left = 750
 	camera.limit_right = 750
 	camera.limit_top = -15
 	camera.limit_bottom = -15
 	
-
-
-
 func _on_exit_body_entered(body: Node2D) -> void:
 	print("Triggered by:", body.name)
 	get_tree().change_scene_to_packed(scene)
