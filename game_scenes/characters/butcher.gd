ce9e174a9ec16392b7b3006ff382d3dcbb0af39c
Stormium04 <stormystorm04@gmail.com>
2025-12-10 13:59:30 -0500

    added Butcher night 1 dialogue

--- game_scenes/characters/butcher.gd
+++ game_scenes/characters/butcher.gd
@@ -1,7 +1,7 @@
 extends npc
-
 var player_in_range = false
 @export var dialogic_timeline: String = "res://Dialogue stuff/talkbutcher.dtl"
+@export var speak: String = "res://Dialogue stuff/butchernight1.dtl"
 
 func _ready():
 	npc_name = "Cylus"
@@ -10,22 +10,31 @@ func _ready():
 	
 func _physics_process(delta):
 	velocity = Vector2.ZERO
+	
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("Interact"):
 		interact()
-
+		
 func _on_area_entered(body):
 	if body.name == "MainCharacter":
 		player_in_range = true
-
+		
 func _on_area_exited(body):
 	if body.name == "MainCharacter":
 		player_in_range = false
-
+		
 func interact():
 	if player_in_range:
-		Dialogic.start(dialogic_timeline)
+		var current_scene = get_tree().current_scene.name
+		
+		if current_scene == "Village":
+			Dialogic.start("res://Dialogue stuff/talkbutcher.dtl")
+		elif current_scene == "NightVillage":
+			Dialogic.start(speak)
+		else:
+			Dialogic.start(dialogic_timeline)  
+			
 		special_talk_function()
-
+		
 func special_talk_function():
 	pass
